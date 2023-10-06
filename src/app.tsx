14ebd0d8e83ebbe40495b82752fcae8dee2db578
TTTPhuong122 <tttp2002@gmail.com>
2023-10-06 23:16:18 +0700

    perf: :green_heart:
    design emplouee account management

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,26 @@
-import { MantineProvider } from '@mantine/core'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { router } from './routes'
-import { RouterProvider } from 'react-router-dom'
+  import { MantineProvider } from '@mantine/core'
+  import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+  import { router } from './routes'
+  import { RouterProvider } from 'react-router-dom'
 
-export default function App() {
-  // Create a client
-  const queryClient = new QueryClient()
-  return (
-    <QueryClientProvider client={queryClient}>
-      <MantineProvider withGlobalStyles withNormalizeCSS
-        theme={{
-          colors: {
-            flashWhite: ['#F0F0F0'],
-            brightGray: ['#EBF2F2'],
-            munsellBlue: ['#0093AF'],
-            cobaltBlue: ['#00439C'],
-          },
-          fontFamily: 'Be Vietnam Pro, sans-serif',
-        }}
-      >
-        <RouterProvider router={router} />
-      </MantineProvider>
-    </QueryClientProvider>
-  )
-}
+  export default function App() {
+    // Create a client
+    const queryClient = new QueryClient()
+    return (
+      <QueryClientProvider client={queryClient}>
+        <MantineProvider withGlobalStyles withNormalizeCSS
+          theme={{
+            colors: {
+              flashWhite: ['#F0F0F0'],
+              brightGray: ['#EBF2F2'],
+              munsellBlue: ['#0093AF'],
+              cobaltBlue: ['#00439C'],
+            },
+            fontFamily: 'Be Vietnam Pro, sans-serif',
+          }}
+        >
+          <RouterProvider router={router} />
+        </MantineProvider>
+      </QueryClientProvider>
+    )
+  }
