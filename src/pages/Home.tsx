1e6163867b354ef3278f18615d1a05d68acd4d6f
TTTPhuong122 <tttp2002@gmail.com>
2023-10-06 23:16:18 +0700

    perf: :green_heart:
    design emplouee account management

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,12 @@
+import Account from "../layouts/Account";
+
 function Home() {
-    return (<>Home</>);
+  return (
+    <>
+      <Account id="1" />
+      {/* <>Home</> */}
+    </>
+  );
 }
 
 export default Home;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,23 @@
-import { createBrowserRouter } from "react-router-dom"
-import Layout from './layouts/Layout'
-import ErrorPage from "./pages/Error"
-import Home from "./pages/Home"
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import ErrorPage from "./pages/Error";
+import Home from "./pages/Home";
+import Account from "./layouts/Account";
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout />,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                index: true,
-                element: <Home />,
-            },
-        ],
-    },
-])
-
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Account />,
+      },
+    ],
+  },
+]);
