1fb22a517372f17511bd81c8aef65d9faccb64a7
chi.ntk <chi.ntk@teko.vn>
2023-11-08 15:50:34 +0700

    Style title delete modal

--- src/utils/deleteModal.tsx
+++ src/utils/deleteModal.tsx
@@ -3,11 +3,11 @@ import { Text } from "@mantine/core";
 
 export const deleteModal = (
   title: string,
-  name: string|undefined,
+  name: string | undefined,
   onConfirm: () => void
 ) => {
   modals.openConfirmModal({
-    title: "Xóa " + title,
+    title: <div style={{ fontWeight: 'bold' }}>{`Xóa ${title}`}</div>,
     centered: true,
     children: <Text size="sm">Bạn có chắc chắn muốn xóa <b>{name}?</b></Text>,
     labels: { confirm: "Xóa", cancel: "Hủy" },
