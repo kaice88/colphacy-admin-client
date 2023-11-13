83ce3b60cca5adc5f0fe23cab54458016589fad7
TTTPhuong122 <tttp2002@gmail.com>
2023-11-13 07:12:06 +0700

    complete delete provider

--- src/hooks/useProvider.ts
+++ src/hooks/useProvider.ts
@@ -1,9 +1,14 @@
 import {
   REQUEST_PROVIDERS,
   REQUEST_PROVIDERS_SEARCH_KEY,
+  REQUEST_PROVIDER_DELETE
 } from "./../constants/apis";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "../settings/axios";
+import { notificationShow } from "../components/Notification";
+import { handleGlobalException } from "../utils/error";
+import { useNavigate } from "react-router-dom";
+import { ErrorObject } from "../types/error";
 function useProvider(
   search: { offset: number; limit: number; keyword: string },
   filter: {
@@ -24,6 +29,7 @@ function useProvider(
 
     return params;
   };
+  const navigate = useNavigate();
   const fetchProvider = useQuery({
     queryKey: ["get-providers"],
     queryFn: () => {
@@ -47,9 +53,34 @@ function useProvider(
       ),
     enabled: false,
   });
+  const handleDeleteProvider = useMutation({
+    mutationKey: ["delete-provider"],
+    mutationFn: (data: { id: number }) => {
+      return axios.delete(REQUEST_PROVIDER_DELETE(data.id));
+    },
+  });
+  const onSubmitDeleteProviderForm = (data: { id: number }) => {
+    handleDeleteProvider.mutate(data, {
+      onSuccess: () => {
+        notificationShow("success", "Success!", "Xóa danh mục thành công!");
+        navigate("/", { state: { from: location.pathname } });
+      },
+      onError: (error) => {
+        const newError = error as ErrorObject
+        handleGlobalException(newError, () => {
+          if (newError.response.status === 400) {
+            const data = newError.response.data;
+            notificationShow("error", "Error!", data.error);
+          }
+        });
+      },
+    });
+  };
   return {
     fetchProvider,
-    fetchProvidersSearchKeywork
+    fetchProvidersSearchKeywork,
+    handleDeleteProvider,
+    onSubmitDeleteProviderForm
   };
 }
 
